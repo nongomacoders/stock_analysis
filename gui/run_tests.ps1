75a0363da9c2c8a30e341d44b9e29233c60154ee
nongomacoders <nongomacoders@gmail.com>
2026-02-20 15:05:33 +0200

    deleted test

--- gui/run_tests.ps1
+++
@@ -1,4 +0,0 @@
-# PowerShell script to run unit tests for GUI
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Set-Location $scriptDir\..\
-python -m unittest discover gui/test -v
--- gui/run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Run tests for the gui package
-cd "$(dirname "$0")" || exit 1
-cd ..
-python -m unittest discover gui/test -v
